842cc1b4d4c78c1f80708408832025662acbd76e
C <christopher.smith@gmx.com>
2020-02-01 16:01:54 +1100

    Generates a list of the capitals

--- scrape.py
+++ scrape.py
@@ -5,23 +5,39 @@ page = requests.get("http://bom.gov.au")
 
 
 soup = BeautifulSoup(page.content, 'html.parser')
-# soup =soup.prettify().encode('utf-8')
+# soup = soup.prettify().encode('utf-8')
+
 
 # print(soup)
 print(soup.title.get_text())
 
 
 # capitals = []
-# capitals = soup.find_all("div", {"class": "capital"})
-# print(list(capitals))
+capitals = soup.find_all("div", {"class": "capital"})
+# print(capitals)
+
+# i = 0
+list_capitals_soup = []
+for div in capitals:
+    # print(div.text.encode('utf-8'))
+    list_capitals_soup.append(div)
+actual_list_of_capitals = []
+for capital in list_capitals_soup:
+    # print capital.find("h3").get_text()
+    actual_list_of_capitals.append(capital.find("h3").get_text())
+
+for capital in actual_list_of_capitals:
+    print(capital)
 
 sydney = soup.find("div", {"class": "capital"})
-# print(sydney)
+# # print(sydney)
 
 now = sydney.find("p", {"class": "now"})
 # print(now)
+
 sydney_now = list(now.children)[1]
 # print(sydney_now)
+
 
 temp = sydney_now.get_text().encode('utf-8')
 
